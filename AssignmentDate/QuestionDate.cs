4c1febe6117845da5d9e481b2a9f68ffe336e838
U_s H_a <ushausama2@outlook.com>
2023-11-21 12:00:12 +0500

    emoved

--- AssignmentDate/QuestionDate.cs
+++ AssignmentDate/QuestionDate.cs
@@ -82,7 +82,7 @@ namespace AssignmentDate
 
 
                     result++;
-                 //   Console.WriteLine(result);
+            
                     
                 }  
             }
